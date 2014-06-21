cf5bccbe1764c224466872770bf0978ba85f8a9f
Marc <marc-git@crazyhaze.de>
2014-06-21 06:48:56 +0200

    version auf 0.1 gesetzt, neorider sagt:die kann jede Community vergeben, wie sie will

--- site.mk
+++ site.mk
@@ -15,7 +15,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')-$(shell git -C $(GLUONDIR)/site log --pretty=format:'%h' -n 1)
+DEFAULT_GLUON_RELEASE := 0.1+0-exp$(shell date '+%Y%m%d')-$(shell git -C $(GLUONDIR)/site log --pretty=format:'%h' -n 1)
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
