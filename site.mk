ca16dad6365cbd666f6b8785be95135a7b31c895
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2014-06-21 02:46:32 +0200

    Commit-Hash des momentanen site-ffmz.git im Imagename. Getestet, funktioniert.

--- site.mk
+++ site.mk
@@ -15,8 +15,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-# TODO: Testen, ob git-log hier wirklich den hash der site-ffmz.git liefert, oder den vom gluon.git
-DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')-$(shell git log --pretty=format:'%h' -n 1)
+DEFAULT_GLUON_RELEASE := 0.4+0-exp$(shell date '+%Y%m%d')-$(shell git -C $(GLUONDIR)/site log --pretty=format:'%h' -n 1)
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
