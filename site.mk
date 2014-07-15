e8b177706973fe626de1ab57214f61ed453cfa8e
Frieder Griesshammer <frieder.griesshammer@der-beweis.de>
2014-07-16 01:18:48 +0200

    Wir haben ein Gluon Packgages Repo!!1! Dies verdient mal einen Sprung im Patchlevel. Der Branch aus dem automatisch gebuildet wird heisst ab sofort autobuild!

--- site.mk
+++ site.mk
@@ -1,11 +1,14 @@
 GLUON_SITE_PACKAGES := \
+#	ffpb-debug \
 	gluon-alfred \
 	gluon-autoupdater \
+	gluon-batman-on-wan \
 	gluon-config-mode \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
 	gluon-luci-admin \
 	gluon-luci-autoupdater \
+	gluon-luci-portconfig \
 	gluon-next-node \
 	gluon-mesh-batman-adv \
 	gluon-mesh-vpn-fastd \
@@ -15,7 +18,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 0.1+0-$(shell date '+%Y.%m.%d-%H.%M')-$(shell git -C $(GLUONDIR) log --pretty=format:'%h' -n 1)-$(shell git -C $(GLUONDIR)/site log --pretty=format:'%h' -n 1)
+DEFAULT_GLUON_RELEASE := 0.1+1-$(shell date '+%Y.%m.%d-%H.%M')-$(shell git -C $(GLUONDIR) log --pretty=format:'%h' -n 1)-$(shell git -C $(GLUONDIR)/site log --pretty=format:'%h' -n 1)
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
