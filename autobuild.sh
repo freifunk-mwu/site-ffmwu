77971831675629e6238ccbad7cd678187d53b1a0
Julian Labus <julian@labus-online.de>
2021-07-16 09:59:56 +0200

    build: bump experimental version to 2021.2

--- autobuild.sh
+++ autobuild.sh
@@ -15,7 +15,7 @@ GLUON_DIR="${SCRIPTPATH}/gluon"
 DATE=$(date +%Y%m%d)
 
 # Overwrite Git Tag for experimental releases
-GLUON_EXP_TAG="2021.1"
+GLUON_EXP_TAG="2021.2"
 
 # Error codes
 E_ILLEGAL_ARGS=126
